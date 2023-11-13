9becd4cecb82c44bb5e60f909170d121380c6d0a
senseiikuiku <ddduc1171@gmail.com>
2023-11-13 20:27:57 +0700

    fix lại css Cart và 1 tí js

--- asset/js/cartt.js
+++ asset/js/cartt.js
@@ -1,53 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const cartItemsList = document.querySelector(".main-left__product");
-  const priceTotalElement = document.getElementById("price-total");
+    const cartItemsList = document.querySelector(".main-left__product");
+    const priceTotalElement = document.getElementById("price-total");
 
-  function displayCartItems(cartItems) {
-    console.log(cartItems);
-    cartItemsList.innerHTML = "";
+    function displayCartItems(cartItems) {
+        console.log(cartItems);
+        cartItemsList.innerHTML = "";
 
-    cartItems.forEach((item) => {
-      const productDiv = createProductDiv(item);
-      cartItemsList.appendChild(productDiv);
-    });
-  }
+        cartItems.forEach((item) => {
+            const productDiv = createProductDiv(item);
+            cartItemsList.appendChild(productDiv);
+        });
+    }
 
-  function createProductDiv(cartItem) {
-    console.log("Creating Product Div: ", cartItem);  
-    const productDiv = document.createElement("div");
-    productDiv.classList.add("main-product");
+    function createProductDiv(cartItem) {
+        console.log("Creating Product Div: ", cartItem);
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("main-product");
 
-    const productContent = `
+        const productContent = `
       <img src="${cartItem.image}" alt="" class="product-image">
-      <div class="product-details">
-        <h2 class="product-title">${cartItem.nameProduct}</h2>
-        <span class="product-price">$${(cartItem.price * cartItem.quantity).toFixed(2)}</span>
-        <span class="product-quantity">x ${cartItem.quantity}</span>
+      <div class="information-product">
+        <div class="product-details">
+          <h2 class="product-title">${cartItem.nameProduct}</h2>
+          <div class="data">
+            <div class="product-price">$${(
+                cartItem.price * cartItem.quantity
+            ).toFixed(2)}</div>
+            <div class="product-quantity">x ${cartItem.quantity}</div>
+          </div>
+        </div>
+        <button class="delete-button" onclick="handleDeleteCartItem(${
+            cartItem.idProduct
+        })">Delete</button>  
       </div>
-      <button class="delete-button" onclick="handleDeleteCartItem(${cartItem.idProduct})">Xóa</button>
     `;
 
-    productDiv.innerHTML = productContent;
+        productDiv.innerHTML = productContent;
 
-    return productDiv;
-  }
+        return productDiv;
+    }
 
-  function updateCartSummary(cartItems) {
-    const totalPrice = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
-    const shippingCost = 10;
-    const totalPriceWithShipping = totalPrice + shippingCost;
+    function updateCartSummary(cartItems) {
+        const totalPrice = cartItems.reduce(
+            (total, item) => total + item.price * item.quantity,
+            0
+        );
+        const shippingCost = 10;
+        const totalPriceWithShipping = totalPrice + shippingCost;
 
-    priceTotalElement.textContent = `$${totalPriceWithShipping.toFixed(2)}`;
-  }
+        priceTotalElement.textContent = `$${totalPriceWithShipping.toFixed(2)}`;
+    }
 
-  // Lấy dữ liệu người dùng từ localStorage
-  const storedDataUsers = localStorage.getItem("DataUsers");
-  const dataUsers = storedDataUsers ? JSON.parse(storedDataUsers) : [];
+    // Lấy dữ liệu người dùng từ localStorage
+    const storedDataUsers = localStorage.getItem("DataUsers");
+    const dataUsers = storedDataUsers ? JSON.parse(storedDataUsers) : [];
 
-  // Lấy giỏ hàng từ dataUsers
-  const user = dataUsers[1]; // Chọn người dùng cụ thể, ví dụ: index 1
-  const cartItems = user.cartItems || [];
+    // Lấy giỏ hàng từ dataUsers
+    const user = dataUsers[1]; // Chọn người dùng cụ thể, ví dụ: index 1
+    const cartItems = user.cartItems || [];
 
-  displayCartItems(cartItems);
-  updateCartSummary(cartItems);
+    displayCartItems(cartItems);
+    updateCartSummary(cartItems);
 });
