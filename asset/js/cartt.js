92f4a5a01caa17e3306b0464bfb12ba25d752715
KayX <truonghuytc@gmail.com>
2023-11-14 22:00:03 +0700

    fixxongcart chua repos

--- asset/js/cartt.js
+++ asset/js/cartt.js
@@ -1,95 +1,175 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const cartItemsList = document.querySelector(".main-left__product");
-    const priceTotalElement = document.getElementById("price-total");
 
-    function displayCartItems(cartItems) {
-        console.log(cartItems);
-        cartItemsList.innerHTML = "";
 
-        cartItems.forEach((item) => {
-            const productDiv = createProductDiv(item);
-            cartItemsList.appendChild(productDiv);
-        });
-    }
+    function render(arr) {
+        const productListContainer = document.querySelector(".main-left__product");
+        productListContainer.innerHTML = "";
 
-    function createProductDiv(cartItem) {
-        console.log("Creating Product Div: ", cartItem);
-        const productDiv = document.createElement("div");
-        productDiv.classList.add("main-product");
+        arr.forEach((product) => {
+            const productDiv = document.createElement("div");
+            productDiv.classList.add("main-product"); // Add the "product" class
 
-        const productContent = `
-            <img src="${cartItem.image}" alt="" class="product-image">
-            <div class="information-product">
-                <div class="product-details">
-                    <h2 class="product-title">${cartItem.nameProduct}</h2>
-                    <div class="data">
-                        <div class="amount-action">
-                            <button class="up-and-down" data-action="decrement"> - </button>
-                            <div class="amount" id="quantity-${cartItem.idProduct}"> ${cartItem.quantity} </div>
-                            <button class="up-and-down" data-action="increment"> + </button>
-                        </div>
-                    </div>
-                </div>
-                <div class="product-price">$${(cartItem.price).toFixed(2)}</div>
-                <button type="button" class="delete-button" onclick="handleDeleteCartItem(${cartItem.idProduct})">Delete</button>
+            productDiv.innerHTML = `
+            <img
+            class="image"
+            src=".${product.image}"
+            alt=""
+        />
+        <div class="info-product">
+            <div class="info-product__title">
+                <h3 class="heading">
+                    ${product.nameProduct}
+                </h3>
+                <div class="price">$${product.price}</div>
             </div>
-        `;
+            <div class="info-product__action">
+                <div class="amount-aciton">
+                    <button class="up-and-down btn-down" onclick = "down(${product.idProduct})">
+                        -
+                    </button>
+                    <div class="amount" id="amount-${product.idProduct}">${product.quantity}</div>
+                    <button class="up-and-down btn-up" onclick = "up(${product.idProduct})">
+                        +
+                    </button>
+                </div>
+                <div class="delete-action">
 
-        productDiv.innerHTML = productContent;
-        const incrementButton = productDiv.querySelector('[data-action="increment"]');
-        const decrementButton = productDiv.querySelector('[data-action="decrement"]');
+                        <img
+                            onclick="deleteProduct(${product.idProduct})"
+                            src="./asset/img/Delete.svg"
+                            alt="Delete"/>
+                    <div class="title"></div>
+                </div>
+            </div>
+        </div>
+            `;
 
-        incrementButton.addEventListener("click", () => updateQuantity(cartItem.idProduct, 1));
-        decrementButton.addEventListener("click", () => updateQuantity(cartItem.idProduct, -1));
-        return productDiv;
+            productListContainer.appendChild(productDiv);
+        });
     }
+    render(dataUsersNow[userIndex].cartItems);
+    function renderPrice() {
+        const totalQuantity = document.querySelector(".total-quantity");
+        console.log(totalQuantity);
+        const price = document.querySelector(".price-checkout");
+        console.log(price);
+        const priceShip = document.querySelector(".price-ship");
+        const totalPrice = document.querySelector(".total-price-full");
 
-    function updateQuantity(productId, quantityChange) {
-        const storedDataUsers = localStorage.getItem("DataUsers");
-        const dataUsers = storedDataUsers ? JSON.parse(storedDataUsers) : [];
+        let sumQuantity = 0;
+        let sumPrice = 0;
+        let shipTotal = 0;
+        dataUsersNow[userIndex].cartItems.forEach((item) => {
+            sumQuantity += item.quantity;
+            sumPrice += item.price * item.quantity;
+            shipTotal += 5 * item.quantity;
+        })
+        totalQuantity.textContent = sumQuantity;
+        price.textContent = sumPrice.toFixed(2);
+        priceShip.textContent = shipTotal;
+        let totalPriceFull = sumPrice + shipTotal;
+        console.log(totalPriceFull);
+        totalPrice.textContent = totalPriceFull.toFixed(2);
+    }
+    renderPrice();
+});
+let dataUsersNow = JSON.parse(localStorage.getItem("DataUsers"));
 
-        const user = dataUsers[1];
-        const updatedCartItems = user.cartItems.map(item => {
-            if (item.idProduct === productId) {
-                const newQuantity = item.quantity + quantityChange;
-                item.quantity = Math.max(newQuantity, 1);
+const loginUserNow = JSON.parse(localStorage.getItem("loginUser"));
+let userIndex = dataUsersNow.findIndex((user) => user.id == loginUserNow.id);
+console.log(dataUsersNow[userIndex]);
+console.log(dataUsersNow[userIndex].cartItems);
+
+function up(idProduct) {
+    const amount = document.getElementById(`amount-${idProduct}`)
+    console.log("fjsadkfasdfdsfsdaf");
+    dataUsersNow[userIndex].cartItems.forEach((item) => {
+        if (item.idProduct == idProduct) {
+            item.quantity++;
+            localStorage.setItem("DataUsers", JSON.stringify(dataUsersNow));
+            const newQuantity = JSON.parse(localStorage.getItem("DataUsers"));
+            newQuantity[userIndex].cartItems.forEach((item) => {
+                if (item.idProduct == idProduct) {
+                    amount.textContent = item.quantity;
+                    renderPriceAfter(newQuantity[userIndex].cartItems);
+                }
+            })
+        }
+    })
+}
+function down(idProduct) {
+    const amount = document.getElementById(`amount-${idProduct}`)
+    dataUsersNow[userIndex].cartItems.forEach((item) => {
+        if (item.idProduct == idProduct) {
+            if (item.quantity > 1) {
+                item.quantity--;
+                localStorage.setItem("DataUsers", JSON.stringify(dataUsersNow));
+                const newQuantity = JSON.parse(localStorage.getItem("DataUsers"));
+                newQuantity[userIndex].cartItems.forEach((item) => {
+                    if (item.idProduct == idProduct) {
+                        amount.textContent = item.quantity;
+                        renderPriceAfter(newQuantity[userIndex].cartItems);
+                    }
+                })
             }
-            return item;
-        });
+        }
+    })
+}
 
-        user.cartItems = updatedCartItems;
-
-        localStorage.setItem("DataUsers", JSON.stringify(dataUsers));
-
-        displayCartItems(updatedCartItems);
-        updateCartSummary(updatedCartItems);
+function deleteProduct(idProduct) {
+    const indexToDelete = dataUsersNow[userIndex].cartItems.findIndex(item => item.idProduct === idProduct);
+    if (indexToDelete !== -1) {
+        const deletedItem = dataUsersNow[userIndex].cartItems.splice(indexToDelete, 1)[0];
+        localStorage.setItem("DataUsers", JSON.stringify(dataUsersNow));
+        const productDiv = document.querySelector(`.main-product #amount-${idProduct}`).closest(".main-product");
+        if (productDiv) {
+            productDiv.remove();
+            renderPriceAfter(deletedItem);
+            render(dataUsersNow[userIndex].cartItems);
+            renderPrice();
+        }
     }
+}
 
-    document.querySelectorAll(".up-and-down").forEach(function (button) {
-        button.addEventListener("click", function () {
-            const productId = button.getAttribute("data-product-id");
-            const quantityChange = parseInt(button.getAttribute("data-quantity-change"));
-            updateQuantity(productId, quantityChange);
-        });
+
+function deleteProduct(idProduct) {
+    const indexToDelete = dataUsersNow[userIndex].cartItems.findIndex(item => item.idProduct === idProduct);
+    if (indexToDelete !== -1) {
+        const deletedItem = dataUsersNow[userIndex].cartItems.splice(indexToDelete, 1)[0];
+        localStorage.setItem("DataUsers", JSON.stringify(dataUsersNow));
+        const productDiv = document.querySelector(`.main-product #amount-${idProduct}`).closest(".main-product");
+        if (productDiv) {
+            productDiv.remove();
+            renderPriceAfter(deletedItem);
+            render(dataUsersNow[userIndex].cartItems);
+            renderPrice();
+        }
+    }
+}
+
+
+function renderPriceAfter(deletedItem) {
+    const totalQuantity = document.querySelector(".total-quantity");
+    const price = document.querySelector(".price-checkout");
+    const priceShip = document.querySelector(".price-ship");
+    const totalPrice = document.querySelector(".total-price-full");
+
+    let sumQuantity = 0;
+    let sumPrice = 0;
+    let shipTotal = 0;
+    dataUsersNow[userIndex].cartItems.forEach((item) => {
+        sumQuantity += item.quantity;
+        sumPrice += item.price * item.quantity;
+        shipTotal += 5 * item.quantity;
     });
 
-    function updateCartSummary(cartItems) {
-        const totalPrice = cartItems.reduce(
-            (total, item) => total + item.price * item.quantity,
-            0
-        );
-        const shippingCost = 10;
-        const totalPriceWithShipping = totalPrice + shippingCost;
+    totalQuantity.textContent = sumQuantity;
+    price.textContent = sumPrice.toFixed(2);
+    priceShip.textContent = shipTotal;
 
-        priceTotalElement.textContent = `$${totalPriceWithShipping.toFixed(2)}`;
-    }
+    let totalPriceFull = sumPrice + shipTotal;
+    console.log(totalPriceFull);
+    sumPrice -= deletedItem.price * deletedItem.quantity;
+    totalPrice.textContent = totalPriceFull.toFixed(2);
+}
 
-    const storedDataUsers = localStorage.getItem("DataUsers");
-    const dataUsers = storedDataUsers ? JSON.parse(storedDataUsers) : [];
-
-    const user = dataUsers[1];
-    const cartItems = user.cartItems || [];
-
-    displayCartItems(cartItems);
-    updateCartSummary(cartItems);
-});
--- asset/js/script.js
+++ asset/js/script.js
@@ -431,7 +431,7 @@ function renderNumberCart(cartItems) {
 
     let totalQuantity = 0; // Tổng số lượng tất cả sản phẩm
     let totalPrice = 0; // Tổng giá tiền tất cả sản phẩm
-    let shippingPrice = 10; // Giá vận chuyển cho mỗi sản phẩm
+    let shippingPrice = 5; // Giá vận chuyển cho mỗi sản phẩm
 
     cartItems.forEach((item) => {
         if (item.check == 0) {
